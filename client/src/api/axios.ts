137c8039ec58f8cc00dc5b2058eca417de0e1c8f
kaneki081 <joseacebuche2@gmail.com>
2023-11-15 01:16:41 +0800

    url changes

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://socia-earist-forum-backend.vercel.app",
+  baseURL: "https://social-earist-forum-backend.vercel.app",
   withCredentials: true,
 });
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 const corsConfig = {
-  origin: "https://lsocia-earist-forum.vercel.app",
+  origin: "https://social-earist-forum.vercel.app",
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
